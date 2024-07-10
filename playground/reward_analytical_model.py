73535d94158b93ee362b9d71fafc9818756a1aa9
Yoav Tzfati <yoav.tzfati@gmail.com>
2024-07-10 02:51:33 -0700

    incorporate scale into reward

--- playground/reward_analytical_model.py
+++ playground/reward_analytical_model.py
@@ -5,9 +5,11 @@ import math
 import matplotlib.pyplot as plt
 
 bad_words_in_game = 4  # As an example
-calibrated_probability = 0.5
 base_probability = lambda: 1 / (bad_words_in_game + 1)
 base_odds = lambda: base_probability() / (1 - base_probability())
+scale = 0.5
+beta = 1
+calibrated_probability = 0.5
 
 
 def log_odds(p: float) -> float:
@@ -15,21 +17,30 @@ def log_odds(p: float) -> float:
 
 
 def reward(n: int) -> float:
+    if n == 0:
+        return 0
     return (
-        (calibrated_probability**n - 1)
-        * (log_odds(calibrated_probability) - log_odds(base_probability()))
-        / (calibrated_probability**n * (calibrated_probability - 1))
-    )
+        reward(n - 1)
+        + beta
+        * (
+            scale * (1 - calibrated_probability)
+            + log_odds(calibrated_probability)
+            - log_odds(base_probability())
+        )
+    ) / calibrated_probability
 
 
 def expected_reward(n: int, p: float) -> float:
-    return p * reward(n)
+    return p * reward(n) - (1 - p) * scale * beta
 
 
 def p_nth_target(n: int, p_accumulated: float, p_pick: float) -> float:
     mu = base_odds() * math.exp(
-        expected_reward(n, p_accumulated * p_pick)
-        - expected_reward(n - 1, p_accumulated)
+        (
+            expected_reward(n, p_accumulated * p_pick)
+            - expected_reward(n - 1, p_accumulated)
+        )
+        / beta
     )
     return mu / (1 + mu)
 
