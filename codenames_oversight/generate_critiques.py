d8ecbc33e1b707b7b7f24250dcf9efa484423e9c
Yoav Tzfati <yoav.tzfati@gmail.com>
2024-08-12 00:40:57 -0400

    fix generate critiques

--- codenames_oversight/generate_critiques.py
+++ codenames_oversight/generate_critiques.py
@@ -1,16 +1,17 @@
 import sys
+from itertools import repeat
 
 import torch
 import typer
-from outlines.generate import regex  # type: ignore
+from outlines.generate import regex, text  # type: ignore
 from outlines.models.transformers import Transformers
-from outlines.samplers import multinomial
 from peft import AutoPeftModelForCausalLM  # type: ignore
 from toolz.itertoolz import partition_all, zip_longest
 from tqdm import tqdm
 from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 
 from .models import GAME_WORDS, Clue, Critique, Game, InferenceSample
+from .oversight import PreferenceSet
 
 app = typer.Typer(pretty_exceptions_show_locals=False)
 
@@ -20,13 +21,19 @@ GENERATION_PATTERN = rf"Critique: {GAME_WORD} > {GAME_WORD}\n"
 
 @app.command()
 def main(
-    model_name_or_path: str = "meta-llama/Llama-2-7b-hf",
-    critiques_per_clue: int = 1,
-    batch_size: int = 12,
+    model_name_or_path: str,
+    critiques_per_clue: int = 2,
+    batch_size: int = 64,
 ):
     "Give some critiques"
+    preference_sets = [PreferenceSet.model_validate_json(line) for line in sys.stdin]
+
     inference_samples = [
-        InferenceSample.model_validate_json(line) for line in sys.stdin
+        InferenceSample(
+            game=p.game, clue_critiques=[o.clue_critiques for o in p.oversights]
+        )
+        for p in preference_sets
+        if all(o.clue_critiques.clue.targets for o in p.oversights)
     ]
     clues_per_game = len(inference_samples[0].clue_critiques)
     quantization_config = BitsAndBytesConfig(load_in_8bit=True)
@@ -53,8 +60,8 @@ def main(
     )
     tokenizer.pad_token = tokenizer.eos_token
     model = Transformers(model, tokenizer)  # type: ignore
-    sampler = multinomial(critiques_per_clue)
-    generator = regex(model, GENERATION_PATTERN, sampler)
+    # generator = regex(model, GENERATION_PATTERN, sampler)
+    generator = text(model)
 
     for batch in tqdm(
         partition_all(batch_size // clues_per_game, inference_samples),
@@ -63,13 +70,15 @@ def main(
     ):
         batch: list[InferenceSample]
         prompts = [
-            format_prompt(sample.game, clue_critique.clue)
+            prompt
             for sample in batch
             for clue_critique in sample.clue_critiques
+            for prompt in repeat(
+                format_prompt(sample.game, clue_critique.clue), critiques_per_clue
+            )
         ]
-        outputs = generator(prompts, max_tokens=32, stop_at="\n")
-        if critiques_per_clue == 1:
-            outputs = [[output] for output in outputs]  # type: ignore
+        outputs = generator(prompts, max_tokens=24, stop_at="\n")
+        outputs = partition_all(critiques_per_clue, outputs)
         for sample, game_outputs in zip_longest(
             batch, partition_all(clues_per_game, outputs)
         ):
@@ -88,7 +97,7 @@ def format_prompt(game: Game, clue: Clue) -> str:
 
 {clue}
 
-"""
+Critique:"""
 
 
 if __name__ == "__main__":
