60948540f8a77a5f13f620d9bf92ac32ff92f644
Yoav Tzfati <yoav.tzfati@gmail.com>
2025-04-19 10:16:17 +0300

    revert server mode

--- results-explorer/astro.config.mjs
+++ results-explorer/astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  adapter: vercel({
+    isr: true,
+  }),
+});
